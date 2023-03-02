d30f7365dc44f098849c317d0c89e7f1d9b49455
AndreFigo <nefigo2001@gmail.com>
2023-03-02 21:33:04 +0000

    Small changes to hough transform

--- T1/matlab/HoughTransform.m
+++ T1/matlab/HoughTransform.m
@@ -3,8 +3,10 @@ function [H, rhoScale, thetaScale] = HoughTransform(Im, threshold, rhoRes, theta
 % HoughTransform - Hough transform of an edge image
 
 % Usage: [H, rhoScale, thetaScale] = HoughTransform(Im, threshold, rhoRes, thetaRes) 
+    
 
-    im = double(Im)/255;
+    im = double(Im)/max(max(Im));
+    %im = double(Im);
     [rows, cols] = size(im);
     maxRho = sqrt((rows-1)^2 + (cols-1)^2);
     %disp(maxRho)
@@ -12,15 +14,17 @@ function [H, rhoScale, thetaScale] = HoughTransform(Im, threshold, rhoRes, theta
     %disp(rhoScale)
     thetaScale = 0:thetaRes:2*pi;	
     H = zeros(length(rhoScale), length(thetaScale));
+    p=0;
     for i = 1:rows
         for j = 1:cols
             if im(i,j) < threshold
                 continue;
             else
+                %disp(1)
                 for k = 1:length(thetaScale)
                     rho = j*cos(thetaScale(k)) + i*sin(thetaScale(k));
                     %disp(rho)
-                    
+                    p = p+1;
                     rhoIndex = round(rho/rhoRes) + ceil(length(rhoScale)/2);
                     
                     %disp(rhoIndex)
@@ -34,11 +38,13 @@ function [H, rhoScale, thetaScale] = HoughTransform(Im, threshold, rhoRes, theta
     %count only positive rho
 
     H = H ( floor(length(rhoScale)/2): end, :);
+    disp(max(max(H)))
     figure;
     imshow(uint8(H/ max(max(H) )* 255));
+    
+    H = uint8(H/ max(max(H) )* 255);
 
-
-
+    disp(p)
 
 end
         
