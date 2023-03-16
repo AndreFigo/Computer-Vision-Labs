cfe02ed225d36fb65c0353438866842c810509cc
Diogo Gouveia <58786427+feguenex@users.noreply.github.com>
2023-03-16 14:18:28 +0000

    inicio keypointDetection

--- T2/matlab/KeypointsDetection.m
+++ T2/matlab/KeypointsDetection.m
@@ -1,5 +1,10 @@
 function [Pts] = KeypointsDetection(Img,Pts)
-%Your implemention here
+    gaussian_k_d = fspecial('gaussian', 2*ceil(3*sigma_d)+1, sigma_d);
+    gaussian_derivative = gradient(gaussian_k_d);
+    
+    Ix = ImageFilter(Img, gaussian_derivative);
+    Iy = ImageFilter(Img, gaussian_derivative');
+    
 
 end
         
